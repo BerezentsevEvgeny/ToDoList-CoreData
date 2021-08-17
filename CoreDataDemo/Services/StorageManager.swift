6cac83ba46628a7432ebac312db4f1623faed9ee
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-17 08:43:06 +0300

    1. Organized files 2. Storage manager added

---
+++ CoreDataDemo/Services/StorageManager.swift
@@ -0,0 +1,18 @@
+//
+//  StorageManager.swift
+//  CoreDataDemo
+//
+//  Created by Евгений Березенцев on 17.08.2021.
+//
+
+import Foundation
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    
+    
+}
--- CoreDataDemo/ViewControllers/TaskListViewController.swift
+++ CoreDataDemo/ViewControllers/TaskListViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import CoreData
 
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
-
 class TaskListViewController: UITableViewController {
 
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -35,7 +31,6 @@ class TaskListViewController: UITableViewController {
         navBarAppearence.configureWithOpaqueBackground()
         navBarAppearence.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        
         navBarAppearence.backgroundColor = UIColor(
             red: 21/255,
             green: 101/255,
@@ -43,22 +38,18 @@ class TaskListViewController: UITableViewController {
             alpha: 194/255
         )
         
+        navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.standardAppearance = navBarAppearence
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearence
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: #selector(addNewTask)
         )
-        
-        navigationController?.navigationBar.tintColor = .white
     }
     
     @objc private func addNewTask() {
-        let newTaskVC = TaskViewController()
-        newTaskVC.delegate = self
-        present(newTaskVC, animated: true)
+        showAlert(with: "New Task", and: "What do you want to do?")
     }
     
     private func fetchData() {
@@ -70,7 +61,44 @@ class TaskListViewController: UITableViewController {
             print(error.localizedDescription)
         }
     }
+    
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        guard let entiyDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else {
+            return
+        }
+        guard let task = NSManagedObject(entity: entiyDescription, insertInto: context) as? Task else { return }
+        task.name = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
+
+//MARK: - DataSource
 
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -87,9 +115,3 @@ extension TaskListViewController {
     }
 }
 
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
-    }
-}
